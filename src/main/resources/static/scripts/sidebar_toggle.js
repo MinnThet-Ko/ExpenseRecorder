18fab42e75051cdcc5b3a5b834d584efc029efb1
MinnThet-Ko <blltspd@gmail.com>
2024-08-07 14:09:25 +0700

    Commit on 07/08/2024
    - created header and sidebar fragments
    - added styles for header and sidebar fragments
    - added icon
    - Voucher insert is operable
    
    Next plan:
    1. Finish up the sidebar navigation.
    2. Implement voucher delete function.
    3. Implement amount transfer feature.

---
+++ src/main/resources/static/scripts/sidebar_toggle.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+	document.getElementsByClassName("sidebar")[0].style.display = "block";
+}
+
+
+function hideSidebar() {
+	document.getElementsByClassName("sidebar")[0].style.display = "none";
+}
