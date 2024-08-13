37674175d68f14a7535f858fc8b1dc1f1a03e9b3
MinnThet-Ko <blltspd@gmail.com>
2024-08-13 10:22:09 +0700

    Commit on 13/08/2024 - modified the header and sidebar sidebar_styles - currently adding transaction types in voucher entry form

--- src/main/resources/static/scripts/sidebar_toggle.js
+++ src/main/resources/static/scripts/sidebar_toggle.js
@@ -6,3 +6,12 @@ function showSidebar() {
 function hideSidebar() {
 	document.getElementsByClassName("sidebar")[0].style.display = "none";
 }
+
+function toggleSidebar() {
+	if (document.getElementsByClassName("sidebar")[0].style.display === "none") {
+		document.getElementsByClassName("sidebar")[0].style.display = "block";
+	} else {
+		document.getElementsByClassName("sidebar")[0].style.display = "none";
+	}
+
+}
